60dc35d896be337ab31ae5da6e6d81e3c8c0cda5
Benjamin DeMann <benjamin.demann@gmail.com>
2024-09-28 15:44:24 -0600

    update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,14 +21,14 @@ const AboutPage = () => {
             <p>
                 I was born and raised in The Church of Jesus Christ of
                 Latter-day Saints, and my faith has been the cornerstone of my
-                life from day one. After serving a mission, I’ve continued to
+                life from day one. After serving a mission, I've continued to
                 live and share my beliefs, currently as a Sunday School teacher
-                in my local ward. Everything I do—how I see the world, what I
-                value, and the way I engage with the world around me—is deeply
-                shaped by my faith. The Book of Mormon, in particular, holds a
-                special place in my heart, not just for its spiritual insights
-                but for its history, translation, and even the geographical
-                discussions it sparks.
+                and president in my local ward. Everything I do—how I see the
+                world, what I value, and the way I engage with the world around
+                me—is deeply shaped by my faith. The Book of Mormon, in
+                particular, holds a special place in my heart, not just for its
+                spiritual insights but for its history, translation, and even
+                the geographical discussions it sparks.
             </p>
             <p>
                 My interest in Freemasonry began during my mission when I
@@ -40,18 +40,18 @@ const AboutPage = () => {
                 understanding that complements my faith in unexpected ways.
             </p>
             <p>
-                Politically, I’m primarily focused on issues that promote and
-                protect families. The Church’s teachings on the family have
-                profoundly influenced my views. I believe that a society’s moral
+                Politically, I'm primarily focused on issues that promote and
+                protect families. The Church's teachings on the family have
+                profoundly influenced my views. I believe that a society's moral
                 foundation and its commitment to families are key to its
                 prosperity—something the Book of Mormon teaches plainly. So long
                 as we are moving toward a society that honors these values,
                 other political debates are, in my mind, secondary.
             </p>
             <p>
-                In my professional life, I’ve been developing software tools for
-                smart contracts for over two years. While I don’t claim to be a
-                cryptocurrency expert, I’m deeply interested in the principles
+                In my professional life, I've been developing software tools for
+                smart contracts for over two years. While I don't claim to be a
+                cryptocurrency expert, I'm deeply interested in the principles
                 behind Web3 and decentralization. I believe in the idea of
                 putting the internet back into the hands of the people, giving
                 us more control over how we interact online.
@@ -60,16 +60,16 @@ const AboutPage = () => {
                 This blog is the product of years of scribbling down thoughts
                 and ideas, refining them, and thinking through them further.
                 Eventually, I decided that it was time to polish those thoughts
-                and share them. I don’t write to provide final answers but to
+                and share them. I don't write to provide final answers but to
                 offer a snapshot of where I am on my journey. My hope is that
                 readers will come along for the ride, engaging with the ideas
                 and feeling empowered to explore and discover for themselves.
             </p>
             <p>
-                If there’s one thing I want you to take away, it’s that faith is
+                If there's one thing I want you to take away, it's that faith is
                 worth holding onto. For those going through a faith crisis, I
-                hope you’ll find here not just reassurance, but a reminder that
-                the journey of faith is one of learning and discovery. There’s
+                hope you'll find here not just reassurance, but a reminder that
+                the journey of faith is one of learning and discovery. There's
                 always more to see, more to know, and more to believe.
             </p>
             <p>
