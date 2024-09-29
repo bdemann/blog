6777baee639fbc36bf1f7dc884feabba202f9ef5
Benjamin DeMann <benjamin.demann@gmail.com>
2024-09-28 21:45:25 -0600

    fixup

--- src/templates/gallery-item.js
+++ src/templates/gallery-item.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { graphql } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import Layout from "../components/layout";
+import { pageHeader } from "../styles/common.module.css";
+import { imageContainer } from "./gallery-item.module.css";
+import Seo from "../components/seo";
+import Fonts from "../components/fonts";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 const GalleryItem = ({ data }) => {
     const { frontmatter } = data.mdx;
-    const image = getImage(frontmatter.image.childImageSharp.gatsbyImageData);
+    const image = getImage(frontmatter.image);
 
     return (
-        <div>
-            <h1>{frontmatter.title}</h1>
-            <GatsbyImage image={image} alt={frontmatter.shortDescription} />
-            <p>{frontmatter.longDescription}</p>
-        </div>
+        <Layout>
+            <h1 className={pageHeader}>{frontmatter.title}</h1>
+            <div className={imageContainer}>
+                <GatsbyImage image={image} alt={frontmatter.shortDescription} />
+                <p>{frontmatter.longDescription}</p>
+            </div>
+        </Layout>
     );
 };
 
