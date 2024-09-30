e33d059680770f1626db955f32ff97ee4a812dd1
Benjamin DeMann <benjamin.demann@gmail.com>
2024-09-30 07:32:14 -0600

    add newly raised

--- src/components/about.js
+++ src/components/about.js
@@ -14,14 +14,15 @@ const About = () => {
             <div className={text}>
                 <p>
                     Born and raised in The Church of Jesus Christ of Latter-day
-                    Saints, I’m a Sunday School teacher who’s passionate about
-                    the Book of Mormon and its teachings. I’m also a Master
-                    Mason, fascinated by the connections between Freemasonry and
-                    the early history of my faith. When I’m not writing about
-                    faith, politics, or Freemasonry, I’m building software tools
-                    for smart contracts and exploring the world of Web3. This
-                    blog is my way of sharing thoughts in progress, inviting
-                    readers to join me on a journey of discovery and reflection.
+                    Saints, I'm a Sunday School teacher who's passionate about
+                    the Book of Mormon and its teachings. I'm also a newly
+                    raised Master Mason, fascinated by the connections between
+                    Freemasonry and the early history of my faith. When I'm not
+                    writing about faith, politics, or Freemasonry, I'm building
+                    software tools for smart contracts and exploring the world
+                    of Web3. This blog is my way of sharing thoughts in
+                    progress, inviting readers to join me on a journey of
+                    discovery and reflection.
                 </p>
             </div>
         </div>
